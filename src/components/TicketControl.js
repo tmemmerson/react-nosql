20bb362d5894f7787d62fdb79c8e58b2dd71c94d
Tristan Emmerson <tristan@stickerslug.com>
2020-09-21 20:13:31 -0700

    complete Monday HW with successful build verified and authorization and authentication implemented

--- src/components/TicketControl.js
+++ src/components/TicketControl.js
@@ -82,44 +82,63 @@ class TicketControl extends React.Component {
   };
 
   render() {
-    let currentlyVisibleState = null;
-    let buttonText = null;
-
-    if (this.state.editing) {
-      currentlyVisibleState = (
-        <EditTicketForm
-          ticket={this.state.selectedTicket}
-          onEditTicket={this.handleEditingTicketInList}
-        />
+    const auth = this.props.firebase.auth();
+    if (!isLoaded(auth)) {
+      return (
+        <React.Fragment>
+          <h1>Loading...</h1>
+        </React.Fragment>
       );
-      buttonText = "Return to Ticket List";
-    } else if (this.state.selectedTicket != null) {
-      currentlyVisibleState = (
-        <TicketDetail
-          ticket={this.state.selectedTicket}
-          onClickingDelete={this.handleDeletingTicket}
-          onClickingEdit={this.handleEditClick}
-        />
-      );
-      buttonText = "Return to Ticket List";
-    } else if (this.props.formVisibleOnPage) {
-      currentlyVisibleState = (
-        <NewTicketForm onNewTicketCreation={this.handleAddingNewTicketToList} />
-      );
-      buttonText = "Return to Ticket List";
-    } else {
-      currentlyVisibleState = (
-        <TicketList onTicketSelection={this.handleChangingSelectedTicket} />
-      );
-      buttonText = "Add Ticket";
     }
-    return (
-      <React.Fragment>
-        {currentlyVisibleState}
-        <button onClick={this.handleClick}>{buttonText}</button>
-      </React.Fragment>
-    );
+    if (isLoaded(auth) && auth.currentUser == null) {
+      return (
+        <React.Fragment>
+          <h1>You must be signed in to access the queue.</h1>
+        </React.Fragment>
+      );
+    }
+    if (isLoaded(auth) && auth.currentUser != null) {
+      let currentlyVisibleState = null;
+      let buttonText = null;
+      if (this.state.editing) {
+        currentlyVisibleState = (
+          <EditTicketForm
+            ticket={this.state.selectedTicket}
+            onEditTicket={this.handleEditingTicketInList}
+          />
+        );
+        buttonText = "Return to Ticket List";
+      } else if (this.state.selectedTicket != null) {
+        currentlyVisibleState = (
+          <TicketDetail
+            ticket={this.state.selectedTicket}
+            onClickingDelete={this.handleDeletingTicket}
+            onClickingEdit={this.handleEditClick}
+          />
+        );
+        buttonText = "Return to Ticket List";
+      } else if (this.props.formVisibleOnPage) {
+        currentlyVisibleState = (
+          <NewTicketForm
+            onNewTicketCreation={this.handleAddingNewTicketToList}
+          />
+        );
+        buttonText = "Return to Ticket List";
+      } else {
+        currentlyVisibleState = (
+          <TicketList onTicketSelection={this.handleChangingSelectedTicket} />
+        );
+        buttonText = "Add Ticket";
+      }
+      return (
+        <React.Fragment>
+          {currentlyVisibleState}
+          <button onClick={this.handleClick}>{buttonText}</button>
+        </React.Fragment>
+      );
+    }
   }
+  // All of the code previously in our render() method should go in this conditional.
 }
 
 /* TicketControl.propTypes = {
